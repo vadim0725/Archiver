e5918a66894f0b832d6069b78253f15e14d2672a
Khun Vadim <hardstwin@gmail.com>
2020-07-23 22:03:24 +0300

    Add class ExitCommand and Interface Command

--- Archiver.java
+++ Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,6 +17,13 @@ public class Archiver {
             String fullPathOpen = ArchPath.readLine();
             zipFileManager.createZip(Paths.get(fullPathOpen));
             } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ExitCommand exitCommand = new ExitCommand();
+        try {
+            exitCommand.execute();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- ConsoleHelper.java
+++ ConsoleHelper.java
@@ -1,33 +1,23 @@
 package com.javarush.task.task31.task3110;
 
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
     public static void writeMessage(String message) {
         System.out.println(message);
     }
 
-    public static String readString() {
-        String ReadStr = null;
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            ReadStr = bufferedReader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ReadStr;
+    public static String readString() throws IOException {
+        String text = bis.readLine();
+        return text;
     }
 
-    public static int readInt() {
-        int ReadInt = 0;
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            ReadInt = Integer.parseInt(bufferedReader.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ReadInt;
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
     }
 }
