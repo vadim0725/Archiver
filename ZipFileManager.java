aab589e7de69b4cac7c3ebeab64f803a70f1a4b8
Khun Vadim <hardstwin@gmail.com>
2020-07-23 18:51:08 +0300

    Realization method creatZip int the ZipFileManager

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,6 +14,16 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
-
+        try (InputStream inputStream = Files.newInputStream(source);
+             ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            while (inputStream.available() != 0) {
+                zipOutputStream.write(inputStream.read());
+            }
+            zipOutputStream.closeEntry();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
