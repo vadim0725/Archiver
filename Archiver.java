4fb700f7e81a05773a478b55d65cd1da290c35de
Khun Vadim <hardstwin@gmail.com>
2020-07-23 17:45:43 +0300

    Add classes Archiver and ZipFileManager

---
+++ Archiver.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) {
+        BufferedReader savePath = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader ArchPath = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String fullPathSave = savePath.readLine();
+            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fullPathSave));
+            String fullPathOpen = ArchPath.readLine();
+            zipFileManager.createZip(Paths.get(fullPathOpen));
+            } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
