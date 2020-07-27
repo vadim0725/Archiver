3525f6417519fc3e8c4674bbd50ae0e1ba2ed93a
Khun Vadim <hardstwin@gmail.com>
2020-07-27 20:11:25 +0300

    Add FileManager class

--- Archiver.java
+++ Archiver.java
@@ -1,15 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
         do {
             try {
@@ -20,33 +17,21 @@ public class Archiver {
             } catch (Exception e) {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
+
         } while (operation != Operation.EXIT);
     }
 
-    public static Operation askOperation() throws IOException {
-        ConsoleHelper.writeMessage("Выберите операцию:\n " +
-                        Operation.CREATE.ordinal() + " - упаковать файлы в архив\n" +
-                        Operation.ADD.ordinal() + "1 - добавить файл в архив\n" +
-                        Operation.REMOVE.ordinal() + "2 - удалить файл из архива\n" +
-                        Operation.EXTRACT.ordinal() + "3 - распаковать архив\n" +
-                        Operation.CONTENT.ordinal() + "4 - просмотреть содержимое архива\n" +
-                        Operation.EXIT.ordinal() + "5 - выход");
 
-        switch (ConsoleHelper.readInt()) {
-            case 0:
-                return Operation.CREATE;
-            case 1:
-                return Operation.ADD;
-            case 2:
-                return Operation.REMOVE;
-            case 3:
-                return Operation.EXTRACT;
-            case 4:
-                return Operation.CONTENT;
-            case 5:
-                return Operation.EXIT;
-            default:
-                throw new IllegalStateException("Unexpected value: " + ConsoleHelper.readInt());
-        }
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
