723989699588ce947d781ff4fbdc655792ea05b0
Khun Vadim <hardstwin@gmail.com>
2020-07-28 19:02:26 +0300

    Add method execute in the ZipContentCommand class

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -15,6 +15,7 @@ import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
+    // Полный путь zip файла
     private final Path zipFile;
 
     public ZipFileManager(Path zipFile) {
@@ -22,23 +23,60 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        // Проверяем, существует ли директория, где будет создаваться архив
+        // При необходимости создаем ее
         Path zipDirectory = zipFile.getParent();
         if (Files.notExists(zipDirectory))
             Files.createDirectories(zipDirectory);
 
+        // Создаем zip поток
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
 
             if (Files.isDirectory(source)) {
+                // Если архивируем директорию, то нужно получить список файлов в ней
                 FileManager fileManager = new FileManager(source);
                 List<Path> fileNames = fileManager.getFileList();
+
+                // Добавляем каждый файл в архив
                 for (Path fileName : fileNames)
                     addNewZipEntry(zipOutputStream, source, fileName);
+
             } else if (Files.isRegularFile(source)) {
+
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
                 addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
             } else {
+
+                // Если переданный source не директория и не файл, бросаем исключение
                 throw new PathIsNotFoundException();
             }
         }
+    }
+
+    public List<FileProperties> getFilesList() throws Exception {
+        // Проверяем существует ли zip файл
+        if (!Files.isRegularFile(zipFile)) {
+            throw new WrongZipFileException();
+        }
+
+        List<FileProperties> files = new ArrayList<>();
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+
+            while (zipEntry != null) {
+                // Поля "размер" и "сжатый размер" не известны, пока элемент не будет прочитан
+                // Давайте вычитаем его в какой-то выходной поток
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                copyData(zipInputStream, baos);
+
+                FileProperties file = new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(), zipEntry.getMethod());
+                files.add(file);
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
+
+        return files;
     }
 
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
@@ -59,24 +97,6 @@ public class ZipFileManager {
         int len;
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
-        }
-    }
-
-    public List<FileProperties> getFilesList() throws Exception {
-        if (Files.isRegularFile(zipFile)) {
-            ArrayList<FileProperties> arrayList = new ArrayList<>();
-            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
-                ZipEntry zipEntry;
-                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    copyData(zipInputStream, out);
-                    arrayList.add(new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(),zipEntry.getMethod()));
-                }
-                zipInputStream.closeEntry();
-            }
-            return arrayList;
-        } else {
-            throw new WrongZipFileException();
         }
     }
 }
--- command/ZipContentCommand.java
+++ command/ZipContentCommand.java
@@ -1,8 +1,16 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import org.omg.CORBA.CODESET_INCOMPATIBLE;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        ConsoleHelper.writeMessage(zipFileManager.getFilesList().toString());
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
